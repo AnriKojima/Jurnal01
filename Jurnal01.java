875e6ee68ff7ce28878a86b873491ea75ce1f378
AnriKojima <145820703+AnriKojima@users.noreply.github.com>
2023-09-27 18:23:22 +0700

    Add files via upload

---
+++ Jurnal01.java
@@ -0,0 +1,11 @@
+public class Jurnal01{
+    public static void main(String[] args) {
+        String nama = "Anri Kojima";
+        String nim = "607062300145";
+        String kelas = "D3IF-47-03";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+    }
+}
